d659dea5aa1afbbacf9930bfabb80702380cce54
Андрей Вязовцев <andrey.vyazovtsev@gmail.com>
2020-11-10 12:11:03 +0300

    Verificate.

--- List.cpp
+++ List.cpp
@@ -4,7 +4,7 @@ int main()
 {
     List lst = {};
     ListConstructor (&lst, 10);
-    lst.elems[1].status = ERROR;
+    printf ("%d", ListVerificate (&lst));
     CreateDump (&lst);
     ListDestructor (&lst);
 
@@ -20,32 +20,157 @@ void ListConstructor (List* lst, const size_t size)
     lst->elems = (Element*) calloc (size, sizeof (Element));
     assert (lst->elems);
 
-    lst->first      = 0;
-    lst->first_null = 1;
-    lst->last       = 0;
-    
-    lst->elems[0].next   = 0;
-    lst->elems[0].prev   = 0;
-    lst->elems[0].num    = NAN;
-    lst->elems[0].status = ZERO;
+    lst->head    = NOT_EXISTING_VERTEX;
+    lst->empty_1 = 0;
+    lst->tail    = NOT_EXISTING_VERTEX;
 
-    for (size_t i_elem = 1; i_elem < size; i_elem++)
-    {
-        lst->elems[i_elem].next   = i_elem + 1;
-        lst->elems[i_elem].prev   = NOT_EXISTING_VERTEX;
-        lst->elems[i_elem].num    = NAN;
-        lst->elems[i_elem].status = EMPTY;
-    }
-    lst->elems[size - 1].next = NOT_EXISTING_VERTEX;
+    for (size_t i_elem = 1; i_elem < size - 1; i_elem++)
+        lst->elems[i_elem] = { NAN, NOT_EXISTING_VERTEX, i_elem + 1, EMPTY };
+
+    lst->elems[0]      = { NAN, NOT_EXISTING_VERTEX,                   1, HEAD_EMPTY };
+    lst->elems[size-1] = { NAN, NOT_EXISTING_VERTEX, NOT_EXISTING_VERTEX, TAIL_EMPTY };
 
     return;
 }
 
-void ListVerificate (List* lst)
+DUMP ListVerificate (List* lst)
+{
+    if (lst == nullptr)
+        return LST_NULL;
+    if (lst->elems == nullptr)
+        return ELEMS_NULL;
+    
+    return ElementsVerificate (lst);
+}
+
+DUMP ElementsVerificate (List* lst)
 {
     assert (lst);
+    assert (lst->elems);
 
-    return;
+    if (lst->size == NOT_EXISTING_VERTEX)
+        return BAD_SIZE;
+    
+    // if (lst->head >= lst->size && lst->head != NOT_EXISTING_VERTEX || ect.)
+
+    if (lst->head + 1 > lst->size                     ||
+        lst->head < lst->size                         &&
+       (lst->elems[lst->head].status != HEAD_N_EMPTY  &&
+        lst->elems[lst->head].status != TAIL_N_EMPTY  ||
+        lst->tail == NOT_EXISTING_VERTEX))
+            return BAD_HEAD;
+
+    if (lst->tail + 1 > lst->size                       ||
+        lst->tail < lst->size                           && 
+       (lst->elems[lst->tail].status != TAIL_N_EMPTY    ||
+        lst->head == NOT_EXISTING_VERTEX))
+            return BAD_TAIL;
+
+    if (lst->empty_1 + 1 > lst->size                    ||
+        lst->empty_1 < lst->size                        &&
+        lst->elems[lst->empty_1].status != HEAD_EMPTY   &&
+        lst->elems[lst->empty_1].status != TAIL_EMPTY)
+            return BAD_EMPTY_1;
+
+    DUMP err = OK;
+
+    for (size_t i_elem = 0; i_elem < lst->size; i_elem++)
+    {
+        switch (lst->elems[i_elem].status)
+        {
+            case EMPTY:
+            case HEAD_EMPTY:
+            case TAIL_EMPTY:
+                if (lst->elems[i_elem].prev != NOT_EXISTING_VERTEX ||
+                    lst->elems[i_elem].next + 1 > lst->size)
+                    {
+                        err = BAD_ELEMS;
+                        lst->elems[i_elem].status = ERROR;
+                    }
+                break;
+
+            case N_EMPTY:
+                if (lst->elems[i_elem].prev >= lst->size ||
+                    lst->elems[i_elem].next >= lst->size ||
+                    isnan (lst->elems[i_elem].num))
+                    {
+                        err = BAD_ELEMS;
+                        lst->elems[i_elem].status = ERROR;
+                    }
+                break;
+
+            case HEAD_N_EMPTY:
+                if (lst->head != i_elem                            ||
+                    lst->elems[i_elem].prev != NOT_EXISTING_VERTEX ||
+                    lst->elems[i_elem].next + 1> lst->size         ||
+                    isnan (lst->elems[i_elem].num))
+                    {
+                        err = BAD_ELEMS;
+                        lst->elems[i_elem].status = ERROR;
+                    }
+                break;
+
+            case TAIL_N_EMPTY:
+                if (lst->tail != i_elem ||
+                    lst->elems[i_elem].next != NOT_EXISTING_VERTEX ||
+                    lst->elems[i_elem].prev + 1 > lst->size        ||
+                    isnan (lst->elems[i_elem].num))
+                    {
+                        err = BAD_ELEMS;
+                        lst->elems[i_elem].status = ERROR;
+                    }
+                break;
+        }
+    }
+
+    // Passing
+
+    if (err != OK)
+        return err;
+
+    size_t passed_elems = 0;
+    
+    if (lst->head != NOT_EXISTING_VERTEX)
+    {
+        size_t next = lst->elems[lst->head].next;
+        passed_elems++;
+
+        while (passed_elems < lst->size && next != NOT_EXISTING_VERTEX)
+        {
+            if (lst->elems[lst->elems[next].prev].next != next)
+                err = PASS_FAILED;
+            next = lst->elems[next].next;
+            passed_elems++;
+        }
+    }
+
+    if (lst->empty_1 != NOT_EXISTING_VERTEX)
+    {
+        if (lst->elems[lst->empty_1].prev != NOT_EXISTING_VERTEX)
+        {
+            err = PASS_FAILED;
+            lst->elems[lst->empty_1].status = ERROR;
+        }
+
+        size_t next = lst->elems[lst->empty_1].next;
+        passed_elems++;
+
+        while (passed_elems < lst->size && next != NOT_EXISTING_VERTEX)
+        {
+            if (lst->elems[next].prev != NOT_EXISTING_VERTEX)
+            {
+                err = PASS_FAILED;
+                lst->elems[next].status = ERROR;
+            }
+            next = lst->elems[next].next;
+            passed_elems++;
+        }
+    }
+
+    if (passed_elems != lst->size)
+        err = PASS_FAILED;
+
+    return err;
 }
 
 void ListDestructor (List* lst)
@@ -55,20 +180,57 @@ void ListDestructor (List* lst)
     free (lst->elems);
     lst->elems = nullptr;
     
-    lst->first      = 0;
-    lst->first_null = 0;
-    lst->last       = 0;
+    lst->head       = 0;
+    lst->empty_1    = 0;
+    lst->tail       = 0;
     lst->size       = 0;
 
     return;
 }
 
-void InsertBefore (List* lst, size_t el, double num, size_t )
+void InsertAllEmpty (List* lst, double num)
 {
-    
+    ass (lst);
+
+    if (lst->head != NOT_EXISTING_VERTEX)
+    {
+        printf ("Call function InsertAllEmpty isn't justified");
+        exit (INSERT_EMPTY);
+    }
+
+    no_empty_elems;
+
+    lst->head = lst->empty_1;
+    lst->tail = lst->empty_1;
+    lst->empty_1 = lst->elems[lst->empty_1].next;
+
+    lst->elems[lst->head].num = num;
+    lst->elems[lst->head].status = TAIL_N_EMPTY;
+
+    ass (lst);
+    return;
 }
 
+void InsertHead (List* lst, double num)
+{
+    ass (lst);
+
+    if (lst->head == NOT_EXISTING_VERTEX)
+    {
+        InsertAllEmpty (lst, num);
+        return;
+    }
+}
+
+void InsertTail (List* lst, double num);
+
+void InsertBefore (List* lst, size_t el, double num);
+
 void InsertAfter (List* lst, size_t el, double num);
+
+void DeleteHead (List* lst);
+
+void DeleteTail (List* lst);
 
 void Delete (List* lst, size_t el);
 
@@ -110,5 +272,24 @@ void CreateDump (List* lst)
     system ("dot -Tpng Graph\\out.dot -o Graph\\gr.png");
     system ("start Graph\\gr.png");
 
+    return;
+}
+
+void TestColors()
+{
+    const size_t COLORS = 6;
+    List lst = {};
+
+    ListConstructor (&lst, COLORS);
+
+    lst.elems[0].status = EMPTY;
+    lst.elems[1].status = HEAD_EMPTY;
+    lst.elems[2].status = N_EMPTY;
+    lst.elems[3].status = HEAD_N_EMPTY;
+    lst.elems[4].status = TAIL_N_EMPTY;
+    lst.elems[5].status = ERROR;
+
+    CreateDump (&lst);
+    ListDestructor (&lst);
     return;
 }
--- List.h
+++ List.h
@@ -5,19 +5,52 @@
 
 #pragma warning (disable : 4996)
 
+#define ass(lst)    DUMP check = ListVerificate (lst);      \
+                    if (check != OK)                        \
+                    {                                       \
+                        printf ("Something went wrong... "  \
+                                "Error number = %d", check);\
+                        ListDestructor (lst);               \
+                        exit (check);                       \
+                    }
+
+#define no_empty_elems  if (lst->empty_1 == NOT_EXISTING_VERTEX)            \
+                        {                                                   \
+                            printf ("Can`t insert the number: no place.");  \
+                            ListDestructor (lst);                           \
+                            exit (check);                                   \
+                        }
+
 const size_t NOT_EXISTING_VERTEX = -1;
 
 enum STATUSES
 {
-    ZERO      = 0xFFFF8A,
-    EMPTY     = 0xB3FFE5,
-    NOT_EMPTY = 0xB0C4D4,
-    ERROR     = 0xFF9494
+    EMPTY           = 0xCCFFCC,
+    HEAD_EMPTY      = 0x66FF66,
+    TAIL_EMPTY      = 0xB2FF66,
+    N_EMPTY         = 0xCCE5FF,
+    HEAD_N_EMPTY    = 0xB266FF,
+    TAIL_N_EMPTY    = 0x66B2FF,
+    ERROR           = 0xFF9494
 };
 
-enum ERRORS
+enum DUMP
 {
+    // List
+    OK              = 0,
+    LST_NULL        = 1,
+    ELEMS_NULL      = 2,
 
+    // Elements
+    BAD_HEAD        = 3,
+    BAD_TAIL        = 4,
+    BAD_EMPTY_1     = 5,
+    BAD_SIZE        = 6,
+    BAD_ELEMS       = 7,
+    PASS_FAILED     = 8,
+
+    //Functions
+    INSERT_EMPTY    = 9
 };
 
 struct Element
@@ -25,23 +58,33 @@ struct Element
     double num = NAN;
     size_t prev = 0;
     size_t next = 0;
-    size_t status = EMPTY;
+    STATUSES status = EMPTY;
 };
 
 struct List
 {
     Element* elems = nullptr;
-    size_t first = 0;
-    size_t last  = 0;
-    size_t size  = 0;
-    size_t first_null = 0;
+    size_t head    = 0;
+    size_t tail    = 0;
+    size_t size    = 0;
+    size_t empty_1 = 0;
 };
 
 void ListConstructor (List* lst, size_t size);
 
-void ListVerificate (List* lst);
+DUMP ListVerificate (List* lst);
+
+DUMP ElementsVerificate (List* lst);
 
 void ListDestructor (List* lst);
+
+void InsertHead (List* lst, double num);
+
+void InsertTail (List* lst, double num);
+
+void InsertHead (List lst, double num);
+
+void InsertTail (List lst, double num);
 
 void InsertBefore (List* lst, size_t el, double num);
 
@@ -50,3 +93,5 @@ void InsertAfter (List* lst, size_t el, double num);
 void Delete (List* lst, size_t el);
 
 void CreateDump (List* lst);
+
+void TestColors();
